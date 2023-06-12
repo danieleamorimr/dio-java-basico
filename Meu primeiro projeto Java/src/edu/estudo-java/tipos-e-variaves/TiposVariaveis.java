1e528a0b43d55ae1a5610018e25ccfe0047ab794
danieleamorimr <danieleamorim10@gmail.com>
2023-06-12 18:45:14 -0300

    Tipos e variáveis

--- Meu primeiro projeto Java/src/edu/estudo-java/tipos-e-variaves/TiposVariaveis.java
+++ Meu primeiro projeto Java/src/edu/estudo-java/tipos-e-variaves/TiposVariaveis.java
@@ -1,3 +1,17 @@
 public class TiposVariaveis {
     
+    public static void main(String[] args) {
+        
+        int numero = 5;
+        numero = 10;
+
+        System.out.println("numero");
+
+       final double VALOR_DE_PI = 3.14;
+		
+        VALOR_DE_PI = 3.15; //não pode ser alterada já que a variável é final.
+    
+    }
+
+   
 }
