ed1ae65683249f0a46074d7cb3f53f476e234444
danieleamorimr <danieleamorim10@gmail.com>
2023-06-12 21:39:33 -0300

    Atribuição, Aritméticos, Ternário, Unitário

---
+++ Meu primeiro projeto Java/src/edu/estudo-java/operadores-java/OperadoresUnarios.java
@@ -0,0 +1,36 @@
+public class OperadoresUnarios {
+    static public void main (String[]args) {
+         //classe Operadores.java
+int numero = 5;
+		
+//Imprimindo o numero negativo
+System.out.println(- numero);
+
+//incrementando numero em mais 1 numero, imprime 6
+numero ++;
+System.out.println(numero);
+
+//incrementando numero em mais 1 numero, imprime 7
+System.out.println(numero ++);// ops algo de errado não está certo
+
+System.out.println(numero);// agora sim, numero virou 7
+
+//ordem de precedencia conta aqui
+System.out.println(++ numero);
+
+numero --;
+System.out.println(numero);
+
+System.out.println(numero --);
+System.out.println(-- numero);
+
+boolean variavel = true;
+
+variavel = !true;
+
+System.out.println(variavel);
+
+    }
+
+  
+}
