db688241116e44e42f63214a8211be72e4d0c8af
danieleamorimr <danieleamorim10@gmail.com>
2023-06-12 22:04:21 -0300

    atualizações

---
+++ Meu primeiro projeto Java/src/edu/estudo-java/operadores/OperadoresTernarios.java
@@ -0,0 +1,14 @@
+public class OperadoresTernarios {
+    static public void main (String[]args) {
+        int a, b;
+            a = 3;
+            b = 5;
+ 
+        String resultado = a==b ? "verdadeiro" : "falso";
+       
+
+        System.out.println(resultado);
+    }
+   
+}
+
