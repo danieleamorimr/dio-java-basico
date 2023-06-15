bab64314688e97ac90cfd1439937a882a691c494
danieleamorimr <danieleamorim10@gmail.com>
2023-06-15 10:27:19 -0300

    java doc

---
+++ Estudos/src/edu/estudo-java/java-doc/Comentarios.java
@@ -0,0 +1,23 @@
+public class Comentarios {
+    // Olá, eu sou um comentário em uma única linha
+
+
+    /* Olá,
+ * Eu sou um comentario
+ * que posso ser mais detalhadod
+ * quando necessário
+ */
+
+ /** 
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+
+
+ /*Um comentário, não possui a finalidade de amenizar um algoritmo não estruturado, 
+ conforme as convenções da linguagem. */
+
+}
